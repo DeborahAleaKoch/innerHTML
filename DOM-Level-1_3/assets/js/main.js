5d5182c2ed4f6a8f317f43d5f6336b1384db7f1b
DeborahAleaKoch <alea-de@web.de>
2024-12-10 17:11:29 +0100

    img size

--- DOM-Level-1_3/assets/js/main.js
+++ DOM-Level-1_3/assets/js/main.js
@@ -4,6 +4,11 @@ const images = document.getElementById("gallery");
 console.log(images);
 
 images.innerHTML =
-  "<figure><img src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk.</figcaption></figure><figure><img src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk1.</figcaption></figure><figure><img src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk2.</figcaption></figure>";
+  "<figure><img src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk.</figcaption></figure><figure><img width='300px' src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk1.</figcaption></figure><figure><img width='300px' src='./assets/img/smallGiftCat.jpg'><figcaption>Geschenk2.</figcaption></figure>";
 
 // images.innerHTML = "<img src='./assets/img/giftCat.jpg'>";
+
+let pictures = document.querySelector("img");
+console.log(pictures);
+
+document.querySelector("img").style.width = "300px";
